82f9c16ecbbe3fcd5e19d1aabffb8b8dfbe1ecc5
DBarry-Code <dabrowski.barry@gmail.com>
2021-05-29 22:10:12 +0200

    added redux, and implemented userReducer and userActions to header and App components

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+
 import "./header.scss";
 
 const Header = ({ currentUser }) => (
@@ -29,4 +31,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapDispatchToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapDispatchToProps)(Header);
